24af7da2e83131ccd230c9fe451b17a8aa32d5ac
salvatore ferlante <salvatore.ferlante@unikorestudent.it>
2024-10-03 00:57:40 +0200

    Fixed missing ENV variable

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Repository update"
 git pull origin develop
 
 echo "Bulding and launching docker container"
-docker-compose -f docker-compose.yml --env-file .env.develop up --build -d
+ENV=develop docker-compose -f docker-compose.yml --env-file .env.develop up --build -d
 
 echo "Active containers"
 docker ps
